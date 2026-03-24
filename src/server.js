74d4f0b6042bfbd29ee2e30ad960cbe57170d828
mbnfo <mbnfox123@gmail.com>
2026-03-24 18:14:01 +0200

    added the render link to the allowed origins

--- src/server.js
+++ src/server.js
@@ -26,6 +26,7 @@ app.use((req, res, next) => {
 const allowedOrigins = [
   'http://localhost:3000',
   'http://localhost:3001',
+  'https://songa.onrender.com/'
 ];
 const corsOptions = {
   origin: (origin, callback) => {
